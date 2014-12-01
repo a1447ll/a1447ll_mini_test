29671d469d7195aed1807cb2796c5242b0116efd
Le Bao Linh <a1447ll@aiit.ac.jp>
2014-12-01 23:38:38 +0900

    Update methods and testcases

---
+++ lib/a1447ll_mini_test/version.rb
@@ -0,0 +1,3 @@
+module A1447llMiniTest
+  VERSION = "0.0.1"
+end
---
+++ test/test_a1447ll_mini_test.rb
@@ -0,0 +1,11 @@
+require 'minitest_helper'
+
+class TestA1447llMiniTest < MiniTest::Unit::TestCase
+  def test_that_it_has_a_version_number
+    refute_nil ::A1447llMiniTest::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
